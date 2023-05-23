8753b0e73a9ab3ffe64952c05f4e1491bdb47097
Thabish <kadertabish2@gmail.com>
2023-05-23 15:39:58 +0400

    font
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- app/components/TextEditor.tsx
+++ app/components/TextEditor.tsx
@@ -21,7 +21,7 @@ export const TextEditor = () => {
 			[{ indent: "-1" }, { indent: "+1" }], // outdent/indent
 			[{ direction: "rtl" }], // text direction
 
-			[{ header: [1, 2, 3, 4, 5, 6, false] }],
+			[{ header: [1, 2, 3, 4, 5, 6, true] }],
 
 			[{ color: [] }, { background: [] }], // dropdown with defaults from theme
 			[{ font: [] }],
@@ -59,7 +59,7 @@ export const TextEditor = () => {
 			onChange={setValue}
 			modules={modules}
 			formats={formats}
-			className=" text-white "
+			className=" text-gray-300"
 		/>
 	);
 };
