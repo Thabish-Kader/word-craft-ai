bf38eb385ad955f3a6c06e827f2a746e0d0d2349
Thabish <kadertabish2@gmail.com>
2023-05-23 12:46:36 +0400

    feature-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- app/components/TextEditor.tsx
+++ app/components/TextEditor.tsx
@@ -2,6 +2,7 @@
 import React, { useMemo, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import "./custom-quill.css";
 import dynamic from "next/dynamic";
 
 export const TextEditor = () => {
@@ -12,12 +13,21 @@ export const TextEditor = () => {
 	);
 	const modules = {
 		toolbar: [
-			[{ header: [1, 2, 3, 4, 5, 6, false] }],
-			[{ font: ["serif", "monospace"] }],
-			["bold", "italic", "underline", "strike", "blockquote"],
+			["bold", "italic", "underline", "strike"], // toggled buttons
+			["blockquote", "code-block"],
+
 			[{ list: "ordered" }, { list: "bullet" }],
-			["link", "image"],
-			["clean"],
+			[{ script: "sub" }, { script: "super" }], // superscript/subscript
+			[{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+			[{ direction: "rtl" }], // text direction
+
+			[{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+			[{ color: [] }, { background: [] }], // dropdown with defaults from theme
+			[{ font: [] }],
+			[{ align: [] }],
+
+			["link", "image", "video"],
 		],
 	};
 
@@ -33,6 +43,13 @@ export const TextEditor = () => {
 		"link",
 		"image",
 		"font",
+		"code-block",
+		"direction",
+		"color",
+		"background",
+		"script",
+		"indent",
+		"align",
 	];
 
 	return (
@@ -42,6 +59,7 @@ export const TextEditor = () => {
 			onChange={setValue}
 			modules={modules}
 			formats={formats}
+			className=""
 		/>
 	);
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { TextEditor } from "./components/TextEditor";
+import { TextEditor2 } from "./components/TextEditor2";
 
 export default function Home() {
 	return (
 		<main className="">
 			<TextEditor />
+			{/* <TextEditor2 /> */}
 		</main>
 	);
 }
