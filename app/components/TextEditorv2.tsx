1c88f5f135cb186ac33570e3629f9da95e2ef2e3
Thabish <kadertabish2@gmail.com>
2023-05-26 15:03:03 +0400

    bug-dix
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- app/components/TextEditorv2.tsx
+++ app/components/TextEditorv2.tsx
@@ -1,72 +1,27 @@
 "use client";
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import "react-quill/dist/quill.snow.css";
 import "./custom-quill.css";
 import axios from "axios";
-import ReactQuill, { Quill } from "react-quill";
-import { extractWordsAfterSlash, stripHtmlTags } from "@/utils/helpers";
-import { FaParagraph } from "react-icons/fa";
-import { CustomButton } from "./CustomButton";
+import ReactQuill from "react-quill";
+import {
+	extractWordsAfterSlash,
+	formats,
+	modules,
+	stripHtmlTags,
+} from "@/utils/helpers";
 
 export const TextEditorv2 = () => {
 	const [value, setValue] = useState("");
 	const [prompt, setPrompt] = useState("");
 
-	var icons = ReactQuill.Quill.import("ui/icons");
+	const icons = ReactQuill.Quill.import("ui/icons");
 	icons["paraphrasebtn"] = `<svg viewbox="0 0 18 18">
     <polygon class="ql-fill ql-stroke" points="6 10 4 12 2 10 6 10"></polygon>
     <path class="ql-stroke" d="M8.09,13.91A4.6,4.6,0,0,0,9,14,5,5,0,1,0,4,9"></path>
   </svg>`;
 
 	const editorRef = React.useRef<ReactQuill>(null);
-
-	const paraphrase = () => {
-		console.log("Button clicked");
-	};
-
-	const modules = {
-		toolbar: {
-			container: [
-				["bold", "italic", "underline", "strike"],
-				["blockquote", "code-block"],
-				[{ list: "ordered" }, { list: "bullet" }],
-				[{ script: "sub" }, { script: "super" }],
-				[{ indent: "-1" }, { indent: "+1" }],
-				[{ direction: "rtl" }],
-				[{ header: [1, 2, 3, 4, 5, 6, true] }],
-				[{ color: [] }, { background: [] }],
-				[{ font: [] }],
-				[{ align: [] }],
-				["link", "image", "video"],
-				["paraphrasebtn"],
-			],
-			handlers: {
-				paraphrasebtn: paraphrase,
-			},
-		},
-	};
-
-	const formats = [
-		"header",
-		"bold",
-		"italic",
-		"underline",
-		"strike",
-		"blockquote",
-		"list",
-		"bullet",
-		"link",
-		"image",
-		"font",
-		"code-block",
-		"direction",
-		"color",
-		"background",
-		"script",
-		"indent",
-		"align",
-		"paraphrase-btn",
-	];
 
 	const handleKeyDown = async (event: React.KeyboardEvent) => {
 		if (event.key === "Tab") {
--- utils/helpers.ts
+++ utils/helpers.ts
@@ -12,3 +12,51 @@ export const stripHtmlTags = (html: string): string => {
 	tmp.innerHTML = html;
 	return tmp.textContent || tmp.innerText || "";
 };
+
+export const paraphrase = () => {
+	console.log("Button clicked");
+};
+
+export const modules = {
+	toolbar: {
+		container: [
+			["bold", "italic", "underline", "strike"],
+			["blockquote", "code-block"],
+			[{ list: "ordered" }, { list: "bullet" }],
+			[{ script: "sub" }, { script: "super" }],
+			[{ indent: "-1" }, { indent: "+1" }],
+			[{ direction: "rtl" }],
+			[{ header: [1, 2, 3, 4, 5, 6, true] }],
+			[{ color: [] }, { background: [] }],
+			[{ font: [] }],
+			[{ align: [] }],
+			["link", "image", "video"],
+			["paraphrasebtn"],
+		],
+		handlers: {
+			paraphrasebtn: paraphrase,
+		},
+	},
+};
+
+export const formats = [
+	"header",
+	"bold",
+	"italic",
+	"underline",
+	"strike",
+	"blockquote",
+	"list",
+	"bullet",
+	"link",
+	"image",
+	"font",
+	"code-block",
+	"direction",
+	"color",
+	"background",
+	"script",
+	"indent",
+	"align",
+	"paraphrasebtn",
+];
